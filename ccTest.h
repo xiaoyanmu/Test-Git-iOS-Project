ae9368948041864d3fa65acbe1744f0aad3af222
xiaoyan wang <redfox21st2000@yahoo.com>
2014-04-28 11:04:33 -0500

    added a test class

---
+++ ccTest.h
@@ -0,0 +1,13 @@
+//
+//  ccTest.h
+//  GitTest
+//
+//  Created by xiaoyan wang on 4/28/14.
+//  Copyright (c) 2014 marquette. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ccTest : NSObject
+
+@end
---
+++ ccTest.m
@@ -0,0 +1,13 @@
+//
+//  ccTest.m
+//  GitTest
+//
+//  Created by xiaoyan wang on 4/28/14.
+//  Copyright (c) 2014 marquette. All rights reserved.
+//
+
+#import "ccTest.h"
+
+@implementation ccTest
+
+@end
